8fed42e686700dbf58fca0e5533c251d9432b953
ArthuuRRRRR <d.delhaye.arthur@gmail.com>
2026-02-17 05:19:44 +0100

    debut de la classe recherche aleatoire

--- recherche_aleatoire.py
+++ recherche_aleatoire.py
@@ -1,2 +1,28 @@
 import numpy as np
 
+class RechercheAleatoire:
+
+    def __init__ (self, n_iter, fonction):
+        self.n_iter = n_iter
+        self.fonction = fonction
+        
+
+    
+    def run(self):
+        meilleur_x = None
+        meilleur_solution = float('inf')
+        history = []
+
+        for i in range(self.n_iter):
+            x1 = np.random.uniform(0.05, 2.0)
+            x2 = np.random.uniform(0.25, 1.3)
+            x3 = np.random.uniform(2.0, 15.0)
+
+            valeurs_test = (x1, x2, x3)
+
+            if self.fonction(valeurs_test) < meilleur_solution:
+                meilleur_solution = self.fonction(valeurs_test)
+                meilleur_x = valeurs_test
+            history.append((i, meilleur_solution))
+
+        return meilleur_x, meilleur_solution, history
